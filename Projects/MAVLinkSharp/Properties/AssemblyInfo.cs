61b6f0c561fed172e1ff4b9b8017ff6665997004
Eduardo Dias da Costa <eduardo.costa@toptal.com>
2023-11-17 03:49:32 -0300

    Project: Reposition of source codes to support unity's assembly definitions

--- Projects/MAVLinkSharp/Properties/AssemblyInfo.cs
+++ Projects/MAVLinkSharp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MAVLinkSharp_")]
+[assembly: AssemblyTitle("MAVLinkSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MAVLinkSharp_")]
+[assembly: AssemblyProduct("MAVLinkSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
